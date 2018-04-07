b91f63838729ce8476039f28c2245e1fefab9d7c
Joseph Martinsen <joseph@martinsen.com>
2018-04-07 14:49:49 -0500

    Upgrade webpack to 4.5

--- src/content_script.ts
+++ src/content_script.ts
@@ -3,6 +3,7 @@ import * as $ from 'jquery';
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.run) {
+            console.log('boo');
             request.CRNS.map((crn, i) => {
                 $(`#crn_id${i + 1}`).val(crn);
             });
